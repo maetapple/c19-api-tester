6520295ee934357afca3a98baa2a9cc7ff2ac24b
Kenta-Nakayama <lucewhite509@gmail.com>
2020-06-24 18:50:01 +0900

    中山作業

--- prefectures_info/lib/main.dart
+++ prefectures_info/lib/main.dart
@@ -9,13 +9,14 @@ import 'dart:math';
  * COVID-19に関する、都道府県毎のデータをJSONで取得します
  */
 Future<PrefectureResponse> fetch() async {
-  final response = await http
-      .get('https://covid19-japan-web-api.now.sh/api/v1/prefectures');
+  final response =
+      await http.get('https://covid19-japan-web-api.now.sh/api/v1/prefectures');
 
   if (response.statusCode != 200) {
-    throw Exception('Failed prefecture info fetching.\nstatus code:${response.statusCode}');
+    throw Exception(
+        'Failed prefecture info fetching.\nstatus code:${response.statusCode}');
   }
-  
+
   return PrefectureResponse.fromJson(json.decode(response.body));
 }
 
@@ -23,23 +24,19 @@ Future<PrefectureResponse> fetch() async {
  * APIレスポンスクラス. JSONデータの形式に合わせて定義します
  * データ数分のPrefecture配列を保持します
  */
-class PrefectureResponse
-{
+class PrefectureResponse {
   List<Prefecture> _prefectures;
 
-  PrefectureResponse( List<dynamic> source )
-  {
+  PrefectureResponse(List<dynamic> source) {
     _prefectures = List();
-    for ( Map<String, dynamic> item in source )
-    {
+    for (Map<String, dynamic> item in source) {
       Prefecture prefecture = Prefecture.fromJson(item);
       _prefectures.add(prefecture);
     }
   }
 
   // Named constructor
-  factory PrefectureResponse.fromJson(List<dynamic> json)
-  {
+  factory PrefectureResponse.fromJson(List<dynamic> json) {
     return PrefectureResponse(json);
   }
 }
@@ -105,7 +102,6 @@ class MyHomePage extends StatefulWidget {
   _APIRequestState createState() => _APIRequestState();
 }
 
-
 /*
  * State開始時にAPI実行してデータを取得します.
  * API実行が終了したら  FutureBuilder<PrefectureResponse> の (context, snapshot) がコールバックされます.
@@ -113,13 +109,20 @@ class MyHomePage extends StatefulWidget {
  * Hot Reload する度に build() が実行されるので県名表示が切り替わります
  */
 class _APIRequestState extends State<MyHomePage> {
-
   Future<PrefectureResponse> futurePrefecture;
+  List<Prefecture> data;
+  bool _active = false;
 
   @override
   void initState() {
     super.initState();
     futurePrefecture = fetch();
+  }
+
+  void _handlePressed() {
+    setState(() {
+      _active = !_active;
+    });
   }
 
   @override
@@ -133,23 +136,86 @@ class _APIRequestState extends State<MyHomePage> {
         appBar: AppBar(
           title: Text('COVID-19 Prefecture Info'),
         ),
-        body: Center(
-          child: FutureBuilder<PrefectureResponse>(
-            future: futurePrefecture,
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                int index = Random.secure().nextInt(snapshot.data._prefectures.length - 1);
-                String name = snapshot.data._prefectures[index].name;
-                return Text(name);
-              }
-              else if (snapshot.hasError) {
-                return Text("${snapshot.error}");
-              }
+        body: Container(
+          child: Column(
+            children: <Widget>[
+              FutureBuilder<PrefectureResponse>(
+                future: futurePrefecture,
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    // int index = Random.secure().nextInt(snapshot.data._prefectures.length - 1);
+                    // String name = snapshot.data._prefectures[index].name;
 
-              return CircularProgressIndicator();
-            },
+                    data = snapshot.data._prefectures;
+
+                    return _viewData(data);
+                    //return Text(name);
+                  } else if (snapshot.hasError) {
+                    return Text("${snapshot.error}");
+                  }
+                  return CircularProgressIndicator();
+                },
+              ),
+              //_refreshButton(), //一覧取得ができるまでは更新ボタンを置いておく。
+            ],
           ),
         ),
+      ),
+    );
+  }
+
+  Widget _viewData(List<Prefecture> data) {
+    return new Expanded(
+      child: ListView.builder(
+          itemCount: data.length,
+          itemBuilder: (BuildContext context, int index) {
+            return GestureDetector(
+              child: Container(
+                decoration: new BoxDecoration(
+                  border: new Border(
+                      bottom: BorderSide(width: 1.0, color: Colors.grey)),
+                ),
+                child: ListTile(
+                  title: Text(
+                    data[index].name,
+                    style: TextStyle(color: Colors.black, fontSize: 18.0),
+                  ),
+                ),
+              ),
+              onTap: (){
+                int temp = index;
+                print(data[temp].name + ' is selected');
+              },
+              onLongPress: (){
+
+              },
+            );
+          }),
+    );
+  }
+
+  /*
+  *一覧取得ができるまでは更新ボタンを置いておく。
+  *※リストですべて表示するようになったので使わない。
+  */
+  Widget _refreshButton() {
+    return Container(
+      margin: EdgeInsets.all(16.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          RaisedButton(
+            onPressed: _handlePressed,
+            color: Colors.blue,
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20.0)),
+            child: const Text(
+              '更新',
+              style: TextStyle(color: Colors.white, fontSize: 20.0),
+            ),
+          ),
+        ],
       ),
     );
   }
